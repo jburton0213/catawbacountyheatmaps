f33457747920d5795a0010653c1bcf0c1243c2dd
Joshua Burton <joshua.burton@greatwide-tm.com>
2020-09-08 10:00:23 -0400

    init

---
+++ .history/src/express/app_20200908065139.ts
@@ -0,0 +1,88 @@
+import express from 'express';
+import { Application } from 'express'
+
+
+var createError = require('http-errors');
+// var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+class App {
+	public app: Application
+	public port: number;
+
+	constructor(appInit: { port: number; middleWares: any; controllers: any; }) {
+		this.app = express()
+		this.port = appInit.port
+
+		this.middlewares(appInit.middleWares)
+		this.routes(appInit.controllers)
+		this.assets()
+		this.template()
+	}
+
+	private middlewares(middleWares: { forEach: (arg0: (middleWare: any) => void) => void; }) {
+        middleWares.forEach(middleWare => {
+            this.app.use(middleWare)
+        })
+    }
+
+    private routes(controllers: { forEach: (arg0: (controller: any) => void) => void; }) {
+        controllers.forEach(controller => {
+            this.app.use('/', controller.router)
+        })
+    }
+
+    private assets() {
+        this.app.use(express.static('public'))
+        this.app.use(express.static('views'))
+    }
+
+    private template() {
+        this.app.set('view engine', 'pug')
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`App listening on the http://localhost:${this.port}`)
+        })
+    }
+
+}
+
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err:any, req:any, res:any, next:any) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
---
+++ .history/src/express/app_20200908070749.ts
@@ -0,0 +1,58 @@
+import express from 'express';
+import { Application } from 'express'
+
+
+var createError = require('http-errors');
+// var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+class App {
+	public app: Application
+	public port: number;
+
+	constructor(appInit: { port: number; middleWares: any; controllers: any; }) {
+		this.app = express()
+		this.port = appInit.port
+
+		this.middlewares(appInit.middleWares)
+		this.routes(appInit.controllers)
+		this.assets()
+		this.template()
+	}
+
+	private middlewares(middleWares: { forEach: (arg0: (middleWare: any) => void) => void; }) {
+        middleWares.forEach(middleWare => {
+            this.app.use(middleWare)
+        })
+    }
+
+    private routes(controllers: { forEach: (arg0: (controller: any) => void) => void; }) {
+        controllers.forEach(controller => {
+            this.app.use('/', controller.router)
+        })
+    }
+
+    private assets() {
+        this.app.use(express.static('public'))
+        this.app.use(express.static('../views'))
+    }
+
+    private template() {
+        this.app.set('view engine', 'pug')
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`App listening on the http://localhost:${this.port}`)
+        })
+    }
+
+}
+
+export default App
